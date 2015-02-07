bb8570ac7114b4269cb35b5b18135c332627521a
Maurice de Beijer <mauricedb@computer.org>
2015-02-07 15:29:08 +0100

    OWIN pipeline

--- ASP.NET vNext Demo/WebDemo/Startup.cs
+++ ASP.NET vNext Demo/WebDemo/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 
@@ -9,6 +10,32 @@ namespace WebDemo
         public void Configure(IApplicationBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+
+
+            //app.Use(async (ctx, next) =>
+            //{
+            //    try
+            //    {
+            //        await next();
+
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        ctx.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            //        await ctx.Response.WriteAsync("Oops: " + ex.Message);
+            //    }
+            //});
+            app.UseErrorPage();
+
+            //app.Use((ctx, next) =>
+            //{
+            //    throw new DivideByZeroException();
+            //});
+
+            //app.Use((ctx, next) => ctx.Response.WriteAsync("<h1>Hello world.</h1>"));
+
+            app.UseWelcomePage();
+
         }
     }
 }
