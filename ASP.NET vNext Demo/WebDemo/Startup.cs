2f627997f88d3226aef8a8b7543b6bb9b518c0f7
Maurice de Beijer <mauricedb@computer.org>
2015-02-08 18:20:36 +0100

    Load IHostingEnvironment

--- ASP.NET vNext Demo/WebDemo/Startup.cs
+++ ASP.NET vNext Demo/WebDemo/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc;
-using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 using WebDemo.Formatters;
@@ -16,6 +15,13 @@ namespace WebDemo
 {
     public class Startup
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         public void Configure(IApplicationBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
@@ -36,10 +42,7 @@ namespace WebDemo
             //});
             app.UseErrorPage();
 
-            //app.Use((ctx, next) =>
-            //{
-            //    throw new DivideByZeroException();
-            //});
+            //app.Use((ctx, next) => { throw new DivideByZeroException(); });
 
             //app.Use((ctx, next) => ctx.Response.WriteAsync("<h1>Hello world.</h1>"));
 
@@ -48,22 +51,19 @@ namespace WebDemo
 
             app.UseServices(svc =>
             {
-                svc.AddSingleton<IBooksRepository, BooksRepository>();
+                svc.AddSingleton<IBooksRepository, BooksRepository>()
+                    .AddMvc()
+                    .Configure<MvcOptions>(options =>
+                    {
+                        var jsonOutputFormatter =
+                            (JsonOutputFormatter)options.OutputFormatters.First(f => f.Instance is JsonOutputFormatter).Instance;
+                        jsonOutputFormatter.SerializerSettings.ContractResolver =
+                            new CamelCasePropertyNamesContractResolver();
 
-                svc.AddMvc();
-                svc.Configure<MvcOptions>(options =>
-                {
-                    var jsonOutputFormatter =
-                        options.OutputFormatters.First(f => f.Instance is JsonOutputFormatter).Instance as
-                            JsonOutputFormatter;
-                    jsonOutputFormatter.SerializerSettings.ContractResolver =
-                        new CamelCasePropertyNamesContractResolver();
-
-                    var serviceDescriptor = svc.Single(s => s.ServiceType == typeof(IHostingEnvironment));
-                    var hostingEnvironment = (IHostingEnvironment)serviceDescriptor.ImplementationFactory(null);
-                    var jpegMediaTypeOutputFormatter = new JpegMediaTypeOutputFormatter(Path.Combine(hostingEnvironment.WebRoot, @"..\Images"));
-                    options.OutputFormatters.Add(jpegMediaTypeOutputFormatter);
-                });
+                        var jpegMediaTypeOutputFormatter =
+                            new JpegMediaTypeOutputFormatter(Path.Combine(_hostingEnvironment.WebRoot, @"..\Images"));
+                        options.OutputFormatters.Add(jpegMediaTypeOutputFormatter);
+                    });
             });
 
             app.UseMvc();
